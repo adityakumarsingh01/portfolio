be8b4c06396ba1cb828e366113ce2c963f61fb4d
Aditya Kumar Singh <adityasingh81201@gmail.com>
2026-08-31 23:56:53 +0530

    chore: increase serverActions bodySizeLimit to 10mb

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.56.1'],
   serverExternalPackages: ['better-sqlite3', '@prisma/adapter-better-sqlite3'],
+  serverActions: {
+    bodySizeLimit: '10mb',
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
